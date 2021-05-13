e53d84c79956c82c1b75506cb5218cd0fcc57ce9
Obafemi Fadairo <obafad94@gmail.com>
2021-05-13 19:49:17 -0400

    figure_out: access individual song lyrics

--- init.py
+++ init.py
@@ -1,6 +1,5 @@
 # NEXT STEPS
 # Ask user for artist and album/song
-# Find out how to parse info that is returned
 
 import os
 import lyricsgenius
@@ -8,7 +7,9 @@ import lyricsgenius
 RAP_GENIUS_ACCESS_TOKEN = os.environ.get('RAP_GENIUS_ACCESS_TOKEN')
 genius = lyricsgenius.Genius(RAP_GENIUS_ACCESS_TOKEN)
 
-artist = genius.search_artist("Cordae", max_songs=1, sort="title")
+# artist = genius.search_artist("Cordae", max_songs=1, sort="title")
 album = genius.search_album("The Lost Boy", "Cordae")
 album.save_lyrics()
-print(album)
+
+for i in album.tracks:
+    print(i.song.lyrics)
