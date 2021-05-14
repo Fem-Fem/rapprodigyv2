9bd2b934df84e8f08645cb09f33d7794a7c3de26
Obafemi Fadairo <obafad94@gmail.com>
2021-05-13 20:50:58 -0400

    feat: add json library to read json data

--- init.py
+++ init.py
@@ -1,19 +1,32 @@
 # NEXT STEPS
 # Ask user for artist and album/song
 
+
+# INSTALLATION STEPS
+# pip install jupyter
+# pip3 install jupyter --user
+
+
+# python -m notebook to open it
+
 import os
 import lyricsgenius
+import json
 
-RAP_GENIUS_ACCESS_TOKEN = os.environ.get('RAP_GENIUS_ACCESS_TOKEN')
-genius = lyricsgenius.Genius(RAP_GENIUS_ACCESS_TOKEN)
+# RAP_GENIUS_ACCESS_TOKEN = os.environ.get('RAP_GENIUS_ACCESS_TOKEN')
+# genius = lyricsgenius.Genius(RAP_GENIUS_ACCESS_TOKEN)
 
-submitted_artist = input("Enter artist: ")
-submitted_album = input("Enter album")
+# submitted_artist = input("Enter artist: ")
+# submitted_album = input("Enter album: ")
 
-album = genius.search_album(submitted_album, submitted_artist)
-album.save_lyrics()
+# album = genius.search_album(submitted_album, submitted_artist)
+# album.save_lyrics()
 
-for i in album.tracks:
-    print(i.song.lyrics)
+# for i in album.tracks:
+#     print(i.song.lyrics)
 
 
+with open('Lyrics_TheLostBoy.json') as f:
+  data = json.load(f)
+
+print(type(data['tracks']))
