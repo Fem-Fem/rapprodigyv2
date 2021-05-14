5fa6481504d69449b572c4b82e9cb3023e2c72ee
Obafemi Fadairo <obafad94@gmail.com>
2021-05-13 22:29:24 -0400

    feat: add storage

--- init.py
+++ init.py
@@ -1,6 +1,9 @@
 # NEXT STEPS
-# Incorporate os.path.exists
 
+# FUTURE
+# Incorporate GCP
+# Markov Model
+# Plotly
 
 # INSTALLATION STEPS
 # pip install jupyter
@@ -11,15 +14,20 @@
 import os
 import lyricsgenius
 import json
+# import code
+# code.interact(local=locals())
 
-# RAP_GENIUS_ACCESS_TOKEN = os.environ.get('RAP_GENIUS_ACCESS_TOKEN')
-# genius = lyricsgenius.Genius(RAP_GENIUS_ACCESS_TOKEN)
 
-# submitted_artist = input("Enter artist: ")
-# submitted_album = input("Enter album: ")
+RAP_GENIUS_ACCESS_TOKEN = os.environ.get('RAP_GENIUS_ACCESS_TOKEN')
+genius = lyricsgenius.Genius(RAP_GENIUS_ACCESS_TOKEN)
 
-# album = genius.search_album(submitted_album, submitted_artist)
-# album.save_lyrics()
+submitted_artist = input("Enter artist: ")
+submitted_album = input("Enter album: ")
+
+if not os.path.exists('Lyrics_' +submitted_album.replace(" ", "")+ ".json"):
+    print("hi")
+    album = genius.search_album(submitted_album, submitted_artist)
+    album.save_lyrics()
 
 # for i in album.tracks:
 #     print(i.song.lyrics)
