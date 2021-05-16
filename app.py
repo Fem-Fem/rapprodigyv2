b020679613f19c9caa12238a97301fbf35bed748
Obafemi Fadairo <obafad94@gmail.com>
2021-05-15 22:23:50 -0400

    fix: add server

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def run(artist_value, album_value):
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div([
     html.H6("Change the artist and value for some fun!"),
